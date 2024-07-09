f3921a730ed2b0c52961721434db8d1c3fe2a9d5
bulieme0 <53142287+bulieme@users.noreply.github.com>
2024-07-09 16:28:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
 ##########################################################
 
 
-demo.launch(
+demo.queue(max_size=10).launch(
         #server_name=args.server_name,
         #server_port=args.server_port,
         share=True,
